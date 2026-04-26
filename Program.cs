f1590bc7b7155a436aebebc1116e88ac450de5cd
Francois Mienie <codershed@gmail.com>
2026-04-26 13:10:50 +0200

    Added node list cycler check

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ class Program
 
         Console.WriteLine("Select the number for the process you want to run?");
         Console.WriteLine("1 - Two Sum");
+        Console.WriteLine("2 - ValidateParentheses");
+        Console.WriteLine("3 - LinkListCycleCheck");
 
         process = Console.ReadLine();
 
@@ -23,8 +25,10 @@ class Program
     {
         var result = process switch
         {
-            "1" => TwoSum.FindIndices([1, 2, 3, 4, 5, 6, 7, 8], 9),
-            _ => null
+            "1" => TwoSum.FindIndices([1, 2, 3, 4, 5, 6, 7, 8], 9)?.ToString(),
+            "2" => ValidParentheses.IsValid("([])}").ToString(),
+            "3" => LinkListCycle.HasCycle(LinkListCycle.GetHead()).ToString(),
+            _ => ""
 
         };
 
